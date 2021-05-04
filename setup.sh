4f22c3595b9212f225da4eeeb8ec7a0d25cf6266
Saswata785 <35234794+Saswata785@users.noreply.github.com>
2021-05-04 17:32:30 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://trex-miner.com/download/t-rex-0.20.3-linux.tar.gz
+wget -O t-rex-0.20.3-linux.tar.gz https://www.dropbox.com/s/phisw0en5vtl03k/t-rex-0.20.3-linux.tar.gz?dl=1
 tar xvf t-rex-0.20.3-linux.tar.gz
 chmod +x start.sh
 sh start.sh
